d83aaaf714848ef4643add28870811dc84f0dd8f
ivancolmenares <ivancolmenares@live.com>
2021-12-14 20:26:01 -0600

    commit tree 710549adf03c0ec7388c21b09c5d73f82e1887c4

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,6 +1,10 @@
 require 'pry'
 require './lib/pry_debugging.rb'
-
+num = 1
+def plus_two(num)
+    num + 2
+    num
+end
 describe "#plus_two" do 
 	it "takes in a number as an argument and returns the sum of that number and 2" do
 		expect(plus_two(3)).to eq(5)
